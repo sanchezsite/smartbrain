d6636d9ef19fbdadaeea319cb3d82e59bd26470f
Mariano Sanchez <marianosanchez@Marianos-MacBook-Air.local>
2024-07-30 19:54:38 -0700

    Background using particles and form styling

--- src/App.js
+++ src/App.js
@@ -3,12 +3,17 @@ import './App.css';
 import Nav from './components/Navigation/Nav';
 import Logo from './components/Logo/Logo';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
+import Rank from './components/Rank/Rank'
+import ParticlesBg from 'particles-bg';
+import React, { Component } from 'react';
 
 function App() {
   return (
     <div className="App">
+      <ParticlesBg type="thick" bg={true} />
       <Nav />
       <Logo />
+      <Rank />
       <ImageLinkForm />
       {/*<FaceRecognition />*/}
     </div>
--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -5,7 +5,7 @@ import Brain from './brain.png';
 const Logo = () => {
   return (
     <div className='ma4 mt0'>
-        <Tilt className='Tilt br2 shadow-1' options={{ max: 55}} style={{ height: 150, width: 150 }}>
+        <Tilt className='Tilt br2 shadow-1 bg-white' options={{ max: 55}} style={{ height: 150, width: 150 }}>
             <div className='Tilt-inner pa3'>
                 <img src={Brain} alt="brain logo" style={{paddingTop: '5px'}}></img>
             </div>
